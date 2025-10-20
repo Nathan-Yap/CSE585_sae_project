bc64ea22dc847ee5b4f9574e4b3ea6f984ce4aba
Patrick Halim <pnhalim@umich.edu>
2025-10-19 22:20:05 -0400

    update verison conflict

---
+++ start.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python3-venv
+sudo apt-get install python3-pip
+
+python3 -m venv env
+source env/bin/activate
+pip install -r greatlakes/basic_pretrained_sae_profiling_one_gpu/requirements.txt
