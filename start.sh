69757d83b182f6a2ddffc1fd629af3383cdc3ea0
Patrick Halim <pnhalim@umich.edu>
2025-10-19 23:59:37 -0400

    Update start script

--- start.sh
+++ start.sh
@@ -3,4 +3,11 @@ sudo apt-get install python3-pip
 
 python3 -m venv env
 source env/bin/activate
+
 pip install -r greatlakes/basic_pretrained_sae_profiling_one_gpu/requirements.txt
+pip uninstall torch torchvision torchaudio
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+sudo apt update
+sudo apt install nvidia-driver-470 nvidia-utils-470
+sudo reboot
